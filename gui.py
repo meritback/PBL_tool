5e5aeb00970ddf9396734436931a1f9513c0b7f4
rengesf <rengesf@remote.cip.ifi.lmu.de>
2021-11-22 13:09:36 +0100

    Gui updatet basic file

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ import cgi
 import cgitb
 import sys
 import os
+import tkinter
 from tkinter import *
 # parse filter options to relevance score
 # filter options as dictionary?
@@ -26,12 +27,29 @@ print("</body>")
 print("</html>")
 """
 
+"""
 class Root(Tk):
     def __init__(self):
         super(Root,self).__init__()
 
         self.title("text mining")
         self.minsize(500,400)
+"""
+window = Tk()
+label = Label(text="Hello World", fg="red", bg="black", width=10, height=1)
+label.pack()
+button = Button(text="Click me!", width=25, height=5,bg="blue",fg="orange")
+button.pack()
+entry = Entry(fg="pink",bg="yellow", width=50)
+input = entry.get()#retreiving text
+entry.delete(0)
+entry.insert(0, "test") #insert String "test" at index 0
+entry.pack()
 
-root = Root()
-root.mainloop()
+#entry.delete()#delete text
+#entry.insert() #insert text
+window.mainloop()
+print(input)
+
+#root = Root()
+#root.mainloop()
