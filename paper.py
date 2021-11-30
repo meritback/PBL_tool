f2ac1e6e0cc611412c33f163ce257441741807a1
glaas <glaas@cip.ifi.lmu.de>
2021-11-30 14:46:02 +0100

    paper class constructor: add scanning for info

--- paper.py
+++ paper.py
@@ -1,19 +1,26 @@
+import urllib.request
 import relevance_score as r
 import numpy as np
-
+import re
 
 # MERIT
 
 class Paper:
-    def __init__(self, title, authors, id, year, count_abstract, count_title):
-        self.title = title
-        self.authors = authors
-        self.id = id
-        self.year = year
-        self.count_abstract = count_abstract
-        self.count_title = count_title
+    def __init__(self, pubmedId):
+
+        # url for pubmed file. see https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.EFetch for more info
+        url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={pubmedId}&retmode=text&rettype=medline'
+        website = urllib.request.urlopen(url).read().decode('utf-8')
+
+        self.id = pubmedId
+        self.title = re.search(r'(?<=TI  - )[\s\S]*?(?=\.\n\S)', website).group().replace('\n      ', ' ') + '.'
+        self.authors = re.findall('(?<=FAU - )[\s\S]*?(?=\n)', website)
+        self.publishDate = re.search('(?<=EDAT- )[\s\S]*?(?= )', website).group()
+
+        abstract = re.search(r'(?<=AB  - )[\s\S]*?(?=\.\n\S)', website).group().replace('\n      ', ' ') + '.'
         self.list = self.list = np.array([self.title, self.authors, self.year, self.id])
         self.score = None
+
 
     def set_score(self, filter_options):
         self.score = r.compute_score(self, filter_options)
